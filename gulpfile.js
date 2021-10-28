44001780aae26f79c4755795c909b1a303a930ed
Kun Lin <link@whitman.edu>
2021-10-27 17:26:32 -0700

    upgrade environment to July 2020 release

--- gulpfile.js
+++ gulpfile.js
@@ -15,5 +15,3 @@ if (options.useScss) config.setUseScss(options.useScss);
 config.setBrowserify(options.browserify);
 config.setSaml(options.saml);
 config.setCas(options.cas);
-
-process.env.NODE_ENV = process.env.NODE_ENV || options.environment || 'production';
